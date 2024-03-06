40f8cd3399a72879d5da649754a88e85a2e9bb14
Omar Moreno <papos21444@gmail.com>
2024-03-05 21:02:27 -0500

    Begin Repository Panter

--- Orders.Backend/obj/Debug/net8.0/Orders.Backend.AssemblyInfo.cs
+++ Orders.Backend/obj/Debug/net8.0/Orders.Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Orders.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+373198773d0220e025b7e3ec8f28f471bde3c5e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ae543244cfb6d3d12bae80c1fb2756d20092527")]
 [assembly: System.Reflection.AssemblyProductAttribute("Orders.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Orders.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Orders.Frontend/Program.cs
+++ Orders.Frontend/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Orders.Frontend;
+using Orders.Frontend.Repositories;
+using System.Security.Cryptography;
+using static System.Net.WebRequestMethods;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5160/") });
+builder.Services.AddScoped<IRepository, Repository>();
 
 await builder.Build().RunAsync();
